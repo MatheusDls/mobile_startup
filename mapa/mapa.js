e777481f1b1e508efcd69fae7c4fd601d9e11bd2
Luis felipe <103533135+Luisfelipeprusch123@users.noreply.github.com>
2024-05-28 00:56:32 -0300

    primeira alteração

---
+++ mapa/mapa.js
@@ -0,0 +1,28 @@
+document.getElementById('home').addEventListener('click', function() {
+    alert('Home clicked');
+});
+
+document.getElementById('search').addEventListener('click', function() {
+    alert('Search clicked');
+});
+
+document.getElementById('settings').addEventListener('click', function() {
+    alert('Settings clicked');
+});
+
+document.getElementById('evolution').addEventListener('click', function() {
+    alert('Evolution clicked');
+});
+
+
+// Dark mode
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+}
+
+
+
+
+
+          
---
+++ script.js
@@ -0,0 +1,37 @@
+document.getElementById('home').addEventListener('click', function() {
+    alert('Home clicked');
+});
+
+document.getElementById('search').addEventListener('click', function() {
+    alert('Search clicked');
+});
+
+document.getElementById('settings').addEventListener('click', function() {
+    alert('Settings clicked');
+});
+
+document.getElementById('evolution').addEventListener('click', function() {
+    alert('Evolution clicked');
+});
+
+
+// Dark mode
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+}
+
+// Paginas
+
+
+  function redirectToSearchPage() {
+            var statisticsPageUrl = "mapa.html";
+
+            window.location.href = statisticsPageUrl;
+        }
+
+
+
+
+
+          
